35cab3931c634af9b6801c696b6841749bc93842
John Anderson <john@maclarenwest.co.uk>
2022-06-17 21:28:25 +0100

    Prep for github

---
+++ dist/dates.d.ts
@@ -0,0 +1,1 @@
+export declare function CheckDateRange(range?: Date[]): (string | null)[];
---
+++ dist/main.d.ts
@@ -0,0 +1,2 @@
+export declare function add(val1: number, val2: number): number;
+export * from "./dates";
